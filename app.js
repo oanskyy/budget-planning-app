5306e7bdc66c4a37f1c4fc9c9a2f237877d16ad2
codessbabe <oana.anghelachi@gmail.com>
2020-10-15 13:44:29 +0300

    Read data from different html input types

--- app.js
+++ app.js
@@ -7,21 +7,38 @@ let budgetController = (function() {
 
 // UI CONTROLLER
 let UIController = (function() { 
-      // some code 
+      let DOMstrings = { 
+            inputType: '.add__type',
+            inputDescription: '.add__description', 
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+      }
+
+      return { 
+            getinput: function() {
+                  return { 
+                        type: document.querySelector(DOMstrings.inputType).value, 
+                        description: document.querySelector(DOMstrings.inputDescription).value, 
+                        value: document.querySelector(DOMstrings.inputValue).value
+                  }
+            }, 
+            getDOMstrings: function() { 
+                  return DOMstrings;
+            }
+      }
       
 })();
 
 
 // GLBAL APP CONTROLLER
-
-
 let controller = (function(budgetCtrl, UICtrl) { 
 
-
+      let DOM = UICtrl.getDOMstrings();
       let ctrlAddItem = function() { 
 
             // 1. get the field input data 
-
+            let input = UICtrl.getinput();
+            console.log(input);
             // 2. add item to budget controller 
 
             // 3. add the new item to the UI 
@@ -32,7 +49,7 @@ let controller = (function(budgetCtrl, UICtrl) {
 
       }
 
-      document.querySelector('.add__btn').addEventListener('click', ctrlAddItem); 
+      document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem); 
 
       document.addEventListener('keypress', function() { 
             if(event.keyCode === 13 || event.which === 13) { 
