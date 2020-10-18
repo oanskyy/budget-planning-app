9208e390d1b42ae8765adde6829a87ad96953e51
codessbabe <oana.anghelachi@gmail.com>
2020-10-18 15:44:21 +0300

    Set up the 'Delete event listener' using event delegation

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ let UIController = (function() {
             budgetLabel: '.budget__value', 
             incomeLabel: '.budget__income--value', 
             expensesLabel: '.budget__expenses--value', 
-            percentageLabel: '.budget__expenses--percentage'
+            percentageLabel: '.budget__expenses--percentage', 
+            container: '.container'
       };
 
       return { 
@@ -134,9 +135,8 @@ let UIController = (function() {
                   } else if (type === 'exp') {
                         element = DOMstrings.expensesContainer;
                         
-                        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
                   }
-
                   
                   // Replace placeholder text with some actual data from OBJ 
                   newHtml = html.replace('%id%', obj.id);
@@ -196,6 +196,10 @@ let controller = (function(budgetCtrl, UICtrl) {
                         ctrlAddItem();
                   }
             }); 
+
+
+            document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
       }
 
       let updateBudget = function() { 
@@ -233,6 +237,31 @@ let controller = (function(budgetCtrl, UICtrl) {
             };
       }
             
+
+      let ctrlDeleteItem = function(event) { 
+            let itemID, splitID, type, ID; 
+
+            if(itemID) { 
+
+                  //inc-1
+                  splitID = itemID.split('-'); 
+                  type = splitID[0];
+                  ID = splitID[1];
+
+                  // 1. Delete the item form the data structure 
+
+
+                  // 2. Delete the item from the UI 
+
+
+                  // 3. Update and show the new budget 
+
+
+                  
+            }
+
+            itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+      }
 
       return { 
             init: function() { 
