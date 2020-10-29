51639831604742beffd4f785e4a9d0f9678c4dd8
codessbabe <oana.anghelachi@gmail.com>
2020-10-29 15:49:00 +0100

    Improve the UX

--- app.js
+++ app.js
@@ -184,6 +184,12 @@ let UIController = (function() {
 
       };
 
+      let nodeListForEach = function(list, callback) { 
+            for (let i = 0; i < list.length; i++) { 
+                  callback(list[i], i);
+            }
+      };
+
       return { 
             getinput: function() {
                   return { 
@@ -258,11 +264,6 @@ let UIController = (function() {
                   
                   let fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
                   
-                  let nodeListForEach = function(list, callback) { 
-                        for (let i = 0; i < list.length; i++) { 
-                              callback(list[i], i);
-                        }
-                  };
 
                   nodeListForEach(fields, function(current, index) { 
                         if(percentags[index] > 0) { 
@@ -285,6 +286,20 @@ let UIController = (function() {
                   document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
             },
+            changedType: function() { 
+                  let fields = document.querySelectorAll(
+                        DOMstrings.inputType + ',' + 
+                        DOMstrings.inputDescription + ',' + 
+                        DOMstrings.inputValue
+                  )
+
+                  nodeListForEach(fields, function(cur) { 
+                        cur.classList.toggle('red-focus');
+                  });
+
+                  document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+            },
+
 
             getDOMstrings: function() { 
                   return DOMstrings;
@@ -308,8 +323,9 @@ let controller = (function(budgetCtrl, UICtrl) {
                   }
             }); 
 
-
+            
             document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+            document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
       }
 
       let updateBudget = function() { 
